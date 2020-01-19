b09ac57210da00e8df8165a018b67ea4b5b99e93
llaumegui <guillaumelef59@gmail.com>
2020-01-20 00:43:45 +0100

    Modif' de la scène de Raph, mise en place de scripts de détections des futurs ennemis / détection des objets avec la caméra Ajout de la méthode 'DrawCircle

---
+++ Matter/Assets/Scripts/EnnemyDetection.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyDetection : MonoBehaviour
+{
+	Transform ennemyTransform;
+
+	[Header ("Ranges")]
+	public float detectionRange;
+	public float attackingRange;
+	public float escapeRange;
+	public float meleeRange;
+
+	[Header ("Assign Player")]
+	public GameObject player;
+	Transform playerTransform;
+
+	bool canDetect;
+	bool canAttack;
+	bool canEscape;
+	bool canMelee;
+	bool enableAction;
+
+	float distance;
+
+	private void Start()
+	{
+		ennemyTransform = GetComponent<Transform>();
+		playerTransform = player.GetComponent<Transform>();
+	}
+
+	private void Update()
+	{
+		if (player != null)
+		{
+			Detection();
+		}
+
+		if (canDetect)
+		{
+			CanDetect();
+		}
+		if (canAttack)
+		{
+			CanAttack();
+		}
+		if (canEscape)
+		{
+			CanEscape();
+		}
+		if (canMelee)
+		{
+			CanMelee();
+		}
+	}
+
+	void Detection()
+	{
+		distance = Vector3.Distance(playerTransform.position, ennemyTransform.position);
+
+		if (distance < detectionRange && distance > attackingRange)
+		{
+			Desactivate();
+			canDetect = true;
+			enableAction = true;
+		}
+		if (distance < attackingRange && distance > escapeRange)
+		{
+			Desactivate();
+			canAttack = true;
+			enableAction = true;
+		}
+		if (distance < escapeRange && distance > meleeRange)
+		{
+			Desactivate();
+			canEscape = true;
+			enableAction = true;
+		}
+		if (distance < meleeRange)
+		{
+			Desactivate();
+			canMelee = true;
+			enableAction = true;
+		}
+		if(distance > detectionRange)
+		{
+			Desactivate();
+		}
+	}
+
+	void Desactivate()
+	{
+		canDetect = false;
+		canAttack = false;
+		canEscape = false;
+		canMelee = false;
+	}
+
+	void CanDetect()
+	{
+		Debug.Log("detecte le joueur");
+	}
+
+	void CanAttack()
+	{
+		Debug.Log("peut attaquer le joueur");
+	}
+
+	void CanEscape()
+	{
+		Debug.Log("fuis le joueur");
+	}
+
+	void CanMelee()
+	{
+		Debug.Log("Attaque de mélée au joueur");
+	}
+
+	//To do : activer Calculing() et Detection() que lorsque le player est à proximité
+}
---
+++ Matter/Assets/Scripts/ShowEnnemyCircles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowEnnemyCircles : MonoBehaviour
+{
+	public EnnemyDetection attachedTo;
+    void Start()
+    {
+		var circle1 = new GameObject { name =  "CircleDetection" };
+		var circle2 = new GameObject { name = "CircleAttacking" };
+		var circle3 = new GameObject { name = "CircleEscaping" };
+		var circle4 = new GameObject { name = "CircleMelee" };
+
+		circle1.transform.position = attachedTo.transform.position;
+		circle2.transform.position = attachedTo.transform.position;
+		circle3.transform.position = attachedTo.transform.position;
+		circle4.transform.position = attachedTo.transform.position;
+
+		circle1.transform.parent = transform;
+		circle2.transform.parent = transform;
+		circle3.transform.parent = transform;
+		circle4.transform.parent = transform;
+
+		circle1.DrawCircle(attachedTo.detectionRange, .5f);
+		circle2.DrawCircle(attachedTo.attackingRange, .5f);
+		circle3.DrawCircle(attachedTo.escapeRange, .5f);
+		circle4.DrawCircle(attachedTo.meleeRange, .5f);
+	}
+}
