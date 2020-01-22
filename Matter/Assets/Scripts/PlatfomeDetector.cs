98b3740812e5e34a62bbb4d282118c6917108769
P3D\RSTELLA <epid.stella.r@gmail.com>
2020-01-22 17:14:23 +0100

    Detection des plateformes avec un Raycast

---
+++ Matter/Assets/Scripts/PlatfomeDetector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatfomeDetector : MonoBehaviour
+{
+    public float Distance;
+    public LayerMask PlatfomeMask;
+    public GameObject cam;
+
+    PlatformeController detect;
+
+    private void Update()
+    {
+        if(Detector())
+        {
+            detect.Detected();
+        }
+    }
+    
+    
+    // A Améliorer pour eviter plusieurs Getcomponent d'affiler.
+    public bool Detector()
+    {
+        if (Physics.Linecast(cam.transform.position,cam.transform.position + transform.forward*Distance,out RaycastHit hitInfo,PlatfomeMask))
+        {
+            hitInfo.transform.gameObject.TryGetComponent(out detect);
+            return true;
+        }
+
+        detect = null;
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(cam.transform.position, cam.transform.position + transform.forward * Distance);
+    }
+}
---
+++ Matter/Assets/Scripts/PlatformeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformeController : MonoBehaviour
+{
+    public PlatfomeDetector detect;
+
+    void Start()
+    {
+        detect = GetComponent<PlatfomeDetector>();
+    }
+
+    public void Detected()
+    {
+        Debug.Log("HelloCoquinouDu59");
+    }
+}
