0f7580706b03d78656b7182b5c50ebf58a3b70db
llaumegui <guillaumelef59@gmail.com>
2020-01-23 23:45:06 +0100

    Ajout de changements de rotation et de feedback visuels pour l'attaque du gardien (Attack V1 gardien "terminée")

--- Matter/Assets/Scripts/Guardian.cs
+++ Matter/Assets/Scripts/Guardian.cs
@@ -18,7 +18,6 @@ public class Guardian : MonoBehaviour
 
 	float distance;
 
-
 	public Transform player;
 	BasePlayer playerScript;
 	Transform thisTransform;
@@ -30,8 +29,26 @@ public class Guardian : MonoBehaviour
 	bool invincible = true;
 	bool alreadyPlaying;
 
+	[Header ("AttackRanges")]
+	GameObject ondeDeChoc;
+	public GameObject attackHitbox;
+
+	[Header("LookAt")]
+	public Transform lookAt;
+	LookTarget lookTarget;
+
 	void Start()
     {
+		ondeDeChoc = new GameObject { name = "OndeDeChocRange" };
+		ondeDeChoc.transform.position = transform.position;
+		ondeDeChoc.transform.parent = transform;
+		ondeDeChoc.DrawCircle(4, 0.2f);
+		ondeDeChoc.SetActive(false);
+
+		attackHitbox.SetActive(false);
+
+		lookTarget = lookAt.gameObject.GetComponent<LookTarget>();
+
 		rendering = GetComponent<MeshRenderer>();
 		rendering.material = standardMaterial;
 		thisTransform = GetComponent<Transform>();
@@ -49,9 +66,10 @@ public class Guardian : MonoBehaviour
 			if (distance >= playerScript.radiusEnnemy && !chargeAttack)
 			{
 				distance = Vector3.Distance(thisTransform.position, player.position);
-				//Quaternion target = 
-				//transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * .2f);
-				thisTransform.position = Vector3.Lerp(thisTransform.position, player.position, Time.deltaTime); //To do : mettre une distance minimale entre l'ennemy et le player
+				lookTarget.LookingTarget();
+				Quaternion target = lookAt.rotation;
+				transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * .2f);
+				thisTransform.position = Vector3.Lerp(thisTransform.position, player.position, Time.deltaTime);
 			}
 			if (distance <= playerScript.radiusEnnemy && !alreadyPlaying)
 			{
@@ -109,20 +127,30 @@ public class Guardian : MonoBehaviour
 		int attackType = Random.Range(1, 3);
 		if(attackType == 1) // onde de choc
 		{
+			Debug.Log("Onde de choc");
 			//anim charge
 			yield return new WaitForSeconds(1f);
 			invincible = false;
 			yield return new WaitForSeconds(0.5f);
+			ondeDeChoc.SetActive(true);
+			yield return new WaitForSeconds(1);
 			invincible = true;
 			attacking = false;
 			chargeAttack = false;
+			ondeDeChoc.SetActive(false);
 		}
 		if(attackType == 2) // normal
 		{
+			distance = Vector3.Distance(thisTransform.position, player.position);
+			lookTarget.LookingTarget();
+			Debug.Log("Attaque normale");
 			//charge
+			yield return new WaitForSeconds(1);
+			attackHitbox.SetActive(true);
 			yield return new WaitForSeconds(1);
 			attacking = false;
 			chargeAttack = false;
+			attackHitbox.SetActive(false);
 		}
 		
 	}
---
+++ Matter/Assets/Scripts/LookTarget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTarget : MonoBehaviour
+{
+	public Transform player;
+	public void LookingTarget()
+	{
+		transform.LookAt(player);
+	}
+}
