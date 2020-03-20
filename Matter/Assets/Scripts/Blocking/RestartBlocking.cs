6e17bbe0390471264f4485247701aa690b52f92e
llaumegui <guillaumelef59@gmail.com>
2020-03-20 13:38:47 +0100

    Implémentation du Blocking dans le projet Unity

---
+++ Matter/Assets/Scripts/Blocking/RestartBlocking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartBlocking : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+	Transform _thisTransform;
+	Vector3 _spawnPosition;
+    void Start()
+    {
+		_thisTransform = GetComponent<Transform>();
+		_spawnPosition = _thisTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+		{
+			_thisTransform.position = _spawnPosition;
+		}
+    }
+}
