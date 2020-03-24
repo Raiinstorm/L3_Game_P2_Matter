a5890c7ab53904e10cfa364dfebc7073db527388
llaumegui <guillaumelef59@gmail.com>
2020-03-24 10:09:08 +0100

    Finalisation du Blocking V1

---
+++ Matter/Assets/Scripts/Blocking/BlockingSpawn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockingSpawn : MonoBehaviour
+{
+	private void Awake()
+	{
+		RestartBlocking.spawns.Add(transform.position);
+	}
+}
--- Matter/Assets/Scripts/Blocking/RestartBlocking.cs
+++ Matter/Assets/Scripts/Blocking/RestartBlocking.cs
@@ -8,10 +8,14 @@ public class RestartBlocking : MonoBehaviour
 
 	Transform _thisTransform;
 	Vector3 _spawnPosition;
+	public static List<Vector3> spawns = new List<Vector3>();
+	int _navigate;
+
     void Start()
     {
 		_thisTransform = GetComponent<Transform>();
 		_spawnPosition = _thisTransform.position;
+		spawns.Add(_spawnPosition);
     }
 
     // Update is called once per frame
@@ -21,5 +25,37 @@ public class RestartBlocking : MonoBehaviour
 		{
 			_thisTransform.position = _spawnPosition;
 		}
-    }
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			if(_navigate-1<0)
+			{
+				_navigate = spawns.Count-1;
+			}
+			else
+			{
+				_navigate--;
+			}
+			Tp();
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			if (_navigate + 1 == spawns.Count)
+			{
+				_navigate = 0;
+			}
+			else
+			{
+				_navigate++;
+			}
+
+			Tp();
+		}
+	}
+
+	void Tp()
+	{
+		_thisTransform.position = spawns[_navigate];
+	}
 }
