8027c8e747ab521c4e1af1ccd21e856aff88aada
Raph_STL <epid.stella.r@gmail.com>
2020-09-25 13:05:18 +0200

    V1.08 : Implémentation du nouveau système de checkpoint

--- Matter/Assets/Scripts/Character/DeathPlayer.cs
+++ Matter/Assets/Scripts/Character/DeathPlayer.cs
@@ -6,36 +6,24 @@ public class DeathPlayer : MonoBehaviour
 {
     PlayerControllerV3 _player;
     [SerializeField] Vector3 _currentlyPostion;
-    [SerializeField] float _timeCheckPos;
-    [SerializeField] float _timeCheckPosTemp;
     [HideInInspector] public bool Respawn;
 
     void Start()
     {
         _player = GetComponent<PlayerControllerV3>();
         _currentlyPostion = _player.transform.position;
-        _timeCheckPosTemp = _timeCheckPos;
         Respawn = false;
-    }
-    private void Update()
-    {
-        if(_timeCheckPosTemp >= 0)
-            _timeCheckPosTemp -= Time.deltaTime;
-        else
-        {
-            CheckPosition();
-            _timeCheckPosTemp = _timeCheckPos;
-        }
-
-    }
-    void CheckPosition()
-    {
-        if (_player.IsGround())
-            _currentlyPostion = _player.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Checkpoint"))
+        {
+            _currentlyPostion = other.gameObject.transform.position;
+            other.gameObject.SetActive(false);
+            // ADD ici l'activation du canvas pour montrer que ça save
+        }
+
         if (other.CompareTag("DeathZone"))
         {
             _player.transform.position = new Vector3(_currentlyPostion.x, _currentlyPostion.y + 0.1f,_currentlyPostion.z) ;
